b77121df08053eb499c8de67080f29f3aaab6958
Kim <13816979+K-Visscher@users.noreply.github.com>
2021-12-04 17:23:50 +0100

    make better use of higher order function for day four

--- src/day_four/part_two.py
+++ src/day_four/part_two.py
@@ -1,63 +1,48 @@
 #!/usr/bin/env python3
+from functools import reduce
 
 
-def calculate_score_of_bingo_card(drawn_numbers, bingo_card):
+def calculate_score(drawn_numbers, bingo_card):
     last_drawn_number = drawn_numbers[-1]
-    sum_of_undrawn_numbers = 0
-
-    for row in bingo_card:
-        sum_of_undrawn_numbers += sum(filter(lambda x: x not in drawn_numbers, row))
-
+    sum_of_undrawn_numbers = reduce(
+        lambda a, row: a + sum(filter(lambda x: x not in drawn_numbers, row)),
+        bingo_card,
+        0,
+    )
     return sum_of_undrawn_numbers * last_drawn_number
 
 
-def is_winning_bingo_card(drawn_numbers, bingo_card):
-    horizontally_winning = False
-    vertically_winning = False
-
-    for horizontal_line in bingo_card:
-        all_numbers_are_drawn_numbers = True
-        for number in horizontal_line:
-            if number not in drawn_numbers:
-                all_numbers_are_drawn_numbers = False
-        if all_numbers_are_drawn_numbers:
-            horizontally_winning = True
-
-    for i in range(len(bingo_card[0])):
-        for vertical_line in [[row[i] for row in bingo_card]]:
-            all_numbers_are_drawn_numbers = True
-            for number in vertical_line:
-                if number not in drawn_numbers:
-                    all_numbers_are_drawn_numbers = False
-            if all_numbers_are_drawn_numbers:
-                vertically_winning = True
-
+def is_winning(drawn_numbers, bingo_card):
+    horizontally_winning = any(
+        map(lambda x: all([n in drawn_numbers for n in x]), bingo_card)
+    )
+    vertically_winning = any(
+        map(
+            lambda x: all([row[x] in drawn_numbers for row in bingo_card]),
+            range(len(bingo_card[0])),
+        )
+    )
     return horizontally_winning or vertically_winning
 
 
 with open("input.txt") as input:
     drawn_numbers = list(map(lambda x: int(x), input.readline().strip().split(",")))
-
-    raw_bingo_card_lines = filter(
-        lambda x: x, map(lambda x: x.strip(), input.readlines())
+    bingo_card_lines = list(
+        map(
+            lambda x: [int(y) for y in x.split(" ") if y],
+            filter(lambda x: x, map(lambda x: x.strip(), input.readlines())),
+        )
     )
-    parsed_bingo_card_lines = list(
-        map(lambda x: [int(y) for y in x.split(" ") if y], raw_bingo_card_lines)
-    )
-
     bingo_cards = [
-        parsed_bingo_card_lines[i : i + 5]
-        for i in range(0, len(parsed_bingo_card_lines), 5)
+        bingo_card_lines[i : i + 5] for i in range(0, len(bingo_card_lines), 5)
     ]
 
     score = None
     winners = set()
     for number_index in range(len(drawn_numbers)):
         for bingo_card_index, bingo_card in enumerate(bingo_cards):
-            if is_winning_bingo_card(drawn_numbers[:number_index], bingo_card):
-                score = calculate_score_of_bingo_card(
-                    drawn_numbers[:number_index], bingo_card
-                )
+            if is_winning(drawn_numbers[:number_index], bingo_card):
+                score = calculate_score(drawn_numbers[:number_index], bingo_card)
                 winners.add(bingo_card_index)
                 if len(winners) == len(bingo_cards):
                     break
